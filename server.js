f450eb423fbc1cb9132a24afbcbf89d139362c45
Parikshat Meena <meenaparikshit2512@gmail.com>
2023-01-08 16:30:40 +0530

    rest api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.Port || process.env.port
+const port = process.env.port
 const host = process.env.host
 
 // const server = http.createServer((req,res)=>{
@@ -15,5 +15,4 @@ const host = process.env.host
 const server = http.createServer(app)
 server.listen(port,()=>{
     console.log(`Server started at port ${host}:${port}`)
-    console.log(`Server started at port ${host}:${ process.env.Port}`)
 })
